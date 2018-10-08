120da4957de5e8251c36d43d43d7c99c90abace6
barry.c.schechter <barry.c.schechter@gmail.com>
2018-10-08 13:56:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments){
+  var emptyArray =[];
+  var mlength = musicians.length;
+  var ilength = instruments.length;
+  var l;
+   for (l = 0; l < mlength; l++) {
+     emptyArray[l] = musicians[l] + " plays "+ instruments[l];
+   }
+   return emptyArray
+}
+function johnLennonFacts(facts){
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + " !!!";
+  }
+  return facts;
+}
